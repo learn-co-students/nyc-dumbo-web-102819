0f8835f97ba084da2cfccae7ebd1a6c0b3e0d720
ihollander <ianholla@gmail.com>
2019-11-08 07:59:33 -0500

    added seeds

---
+++ 09-activerecord-associations/db/seeds.rb
@@ -0,0 +1,14 @@
+# seed file will run Ruby code, we use this to populate our database
+students = [ 
+  "Bihelca", "Conrad", "Dominic", "Eric", "FanZhong", "Frankie",
+  "Griffin", "Guligena", "Imade", "Jakarai", "James", "Kay",
+  "Sadia", "Samir", "Soner", "Tristan"
+]
+
+consoles = [ "Switch", "PS4", "XBox", "PC" ]
+
+# create a Player instance for each student and save it to the database
+students.each do |student|
+  # .sample picks a random item from an array
+  Player.create(name: student, console: consoles.sample, age: rand(1..100))
+end
