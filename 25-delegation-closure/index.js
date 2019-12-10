818500905e0b0e664ff9fbb322e83c6a0e01ab5b
Eric Kim <hyeokjungkim@gmail.com>
2019-12-10 14:11:12 -0500

    adds lecture

---
+++ 25-delegation-closure/index.js
@@ -0,0 +1,33 @@
+// 1) Find the element that we care about
+// 2) Add the event listener on that element using addEventListener
+
+// element.addEventListener("event_type", function(evt){
+  // Most likely, do some DOM Manipulation
+// })
+
+
+let tacoButton = document.querySelector("#taco")
+let emojisUl = document.querySelector("#blobs")
+let newEmojiForm = document.querySelector("#emoji-form")
+
+
+
+newEmojiForm.addEventListener("submit", function(evt){
+  evt.preventDefault()
+  let newEmoji = evt.target.emoji.value
+  // let newEmoji = evt.target["emoji-with-a-dash"]
+
+  let newLi = document.createElement("li")
+  newLi.innerText = newEmoji
+  emojisUl.append(newLi)
+
+  evt.target.reset()
+})
+
+
+
+tacoButton.addEventListener("click", function(evt){
+  let newLi = document.createElement("li")
+  newLi.innerText = "🌮"
+  emojisUl.append(newLi)
+})
